c16c314006763dddb6e31524c5dffb94b2e4b83a
JulienLaurent77 <julienlaurent77@outlook.com>
2022-11-25 15:21:04 +0100

    design form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,23 +7,26 @@ import { AiFillMail } from 'react-icons/ai'
 import ContactForm from './ContactForm'
 
 const Contact = () => {
-  return (
-    <div>
-    <h1> Contact </h1>
-    <p> Notre salon se situe au </p>
-    <p> 332 Rue Louis Gillain <br></br>
-        27210 Beuzeville </p>
-    < GoogleMap />
-    <h2> Suivez-nous ! </h2>
-    <div className="social-icons">
-        < FaTiktok />
-        < BsInstagram />
-        < FaFacebook />
-        < AiFillMail />
-    </div>
-    <ContactForm />
-    </div>
-  )
+    return (
+        <div className='container'>
+            <div className='contact'>
+                <h2> Notre salon se situe au :</h2>
+                <p> 332 Rue Louis Gillain <br></br>
+                    27210 Beuzeville </p>
+            </div>
+            < GoogleMap />
+            <div className='social'>
+                <h2> Suivez-nous ! </h2>
+                <div className="social-icons">
+                <a href='rt'>< FaTiktok /></a>
+                <a href='https://www.instagram.com/julienbourgarit/'>< BsInstagram /></a>
+                <a href='https://www.facebook.com/julienbourgarit/'>< FaFacebook /></a>
+                <a href='rt'>< AiFillMail /></a>
+                </div>
+            </div>
+            <ContactForm />
+        </div>
+    )
 }
 
 export default Contact
