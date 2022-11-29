d779a27b3a6b148688244def5a49e9c2fdfef39f
JulienLaurent77 <julienlaurent77@outlook.com>
2022-11-29 17:00:11 +0100

    initialize blog component

---
+++ src/components/Blog/BlogArticle.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogArticle = () => {
+  return (
+    <div>BlogArticle</div>
+  )
+}
+
+export default BlogArticle
---
+++ src/components/Blog/BlogSummary.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogSummary = () => {
+  return (
+    <div>BlogSummary</div>
+  )
+}
+
+export default BlogSummary
