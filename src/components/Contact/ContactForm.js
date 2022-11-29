67edfe0230695d4aba227307e05eb3fff7b00e16
anthony <a.ouzhene@gmail.com>
2022-11-29 12:38:54 +0100

    adjust fields in all forms + change colors of buttons

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './styles.scss'
+import './contact.scss'
 
 const ContactForm = () => {
     return (
--- src/components/Login/SignUp.js
+++ src/components/Login/SignUp.js
@@ -2,7 +2,27 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='container'>
+      <h1>Inscription</h1>
+
+      <form className="formContainer" action="/action_page.php">
+
+        <label for="pseudo">Pseudo</label>
+        <input id="pseudo" type="email" name="email" placeholder="Votre pseudo" required></input><br></br>
+
+        <label for="emailAddress">Email</label>
+        <input id="emailAddress" type="email" name="email" placeholder="Votre email" required></input><br></br>
+
+        <label for="password">Mot de passe</label>
+        <input id="password" type="password" name="password" placeholder="Votre mot de passe" required></input><br></br>
+
+        <label for="password">Confirmer le mot de passe</label>
+        <input id="password" type="password" name="password" placeholder="Confirmer le mot de passe" required></input><br></br>
+
+        <input type="submit" value="Rejoindre"></input>
+
+      </form>
+    </div>
   )
 }
 
