58e3185db45dd9babe9b26b885fd330d7d4b6d79
JulienLaurent77 <julienlaurent77@outlook.com>
2022-12-01 15:14:57 +0100

    add style to card and pagination

--- src/components/Blog/BlogSummary.js
+++ src/components/Blog/BlogSummary.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './styles.scss'
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
+
 
 import blogImage1 from './images/blog1.jpg'
 import blogImage2 from './images/blog2.jpg'
@@ -10,6 +12,7 @@ const BlogSummary = () => {
   return (
     <div>
       <h1>Blog</h1>
+      <p className='blog-introduction'>Lorem ipsum dolor sit amet. Eum ratione soluta non reprehenderit consequatur vel veniam quia non obcaecati vitae qui labore aspernatur. Nam totam cupiditate et voluptates temporibus sit mollitia alias non optio soluta eos eligendi autem.</p>
       <div className='blog'>
 
         <a href='/actualites/article' className="blog-card" target='_blank' >
@@ -44,6 +47,16 @@ const BlogSummary = () => {
           <h1 className="blog-card--title">L'art du tatouage</h1>
           <p>Lorem ipsum dolor sit amet. Eum ratione soluta non reprehenderit consequatur vel veniam quia non obcaecati vitae qui labore aspernatur. Nam totam cupiditate et voluptates temporibus sit mollitia alias non optio soluta eos eligendi autem. Non voluptatem quia est cumque amet et exercitationem quia ut excepturi exercitationem et deleniti fugit. A animi nulla non ipsa porro et culpa consectetur</p>
         </a>
+      </div>
+
+      <div className="blog-pagination">
+        <button className="blog-pagination-button-left"><FaArrowAltCircleLeft /></button>
+        <div className="blog-pagination-button-number">
+          <button className="blog-pagination-button-number-1">1</button>
+          <button className="blog-pagination-button-number-2">2</button>
+          <button className="blog-pagination-button-number-3">3</button>
+        </div>
+        <button className="blog-pagination-button-right"><FaArrowAltCircleRight /></button>
       </div>
     </div>
   )
