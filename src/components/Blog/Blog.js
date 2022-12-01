2ee4b5ff38482e1eb4f6ee67983ba74fe13fe7f2
JulienLaurent77 <julienlaurent77@outlook.com>
2022-12-01 16:24:28 +0100

    add date et read more on articles cards in blog page

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -25,6 +25,10 @@ const Blog = () => {
           </div>
           <h1 className="blog-card--title">Be yourself</h1>
           <p>Lorem ipsum dolor sit amet. Eum ratione soluta non reprehenderit consequatur vel veniam quia non obcaecati vitae qui labore aspernatur. Nam totam cupiditate et voluptates temporibus sit mollitia alias non optio soluta eos eligendi autem. Non voluptatem quia est cumque amet et exercitationem quia ut excepturi exercitationem et deleniti fugit. A animi nulla non ipsa porro et culpa consectetur</p>
+          <div className="blog-card--info">
+            <p className="blog-card--date">12/12/2020</p>
+            <p className="blog-card--read-more">Lire la suite...</p>
+          </div>
         </a>
 
         <a href='blank' className="blog-card" target='_blank' >
@@ -33,6 +37,10 @@ const Blog = () => {
           </div>
           <h1 className="blog-card--title">Dites le avec sagesse</h1>
           <p>Lorem ipsum dolor sit amet. Eum ratione soluta non reprehenderit consequatur vel veniam quia non obcaecati vitae qui labore aspernatur. Nam totam cupiditate et voluptates temporibus sit mollitia alias non optio soluta eos eligendi autem. Non voluptatem quia est cumque amet et exercitationem quia ut excepturi exercitationem et deleniti fugit. A animi nulla non ipsa porro et culpa consectetur</p>
+          <div className="blog-card--info">
+            <p className="blog-card--date">12/12/2020</p>
+            <p className="blog-card--read-more">Lire la suite...</p>
+          </div>
         </a>
 
 
@@ -42,6 +50,10 @@ const Blog = () => {
           </div>
           <h1 className="blog-card--title">Dépassez-vous !</h1>
           <p>Lorem ipsum dolor sit amet. Eum ratione soluta non reprehenderit consequatur vel veniam quia non obcaecati vitae qui labore aspernatur. Nam totam cupiditate et voluptates temporibus sit mollitia alias non optio soluta eos eligendi autem. Non voluptatem quia est cumque amet et exercitationem quia ut excepturi exercitationem et deleniti fugit. A animi nulla non ipsa porro et culpa consectetur</p>
+          <div className="blog-card--info">
+            <p className="blog-card--date">12/12/2020</p>
+            <p className="blog-card--read-more">Lire la suite...</p>
+          </div>
         </a>
 
         <a href='blank' className="blog-card" target='_blank' >
@@ -50,6 +62,10 @@ const Blog = () => {
           </div>
           <h1 className="blog-card--title">L'art du tatouage</h1>
           <p>Lorem ipsum dolor sit amet. Eum ratione soluta non reprehenderit consequatur vel veniam quia non obcaecati vitae qui labore aspernatur. Nam totam cupiditate et voluptates temporibus sit mollitia alias non optio soluta eos eligendi autem. Non voluptatem quia est cumque amet et exercitationem quia ut excepturi exercitationem et deleniti fugit. A animi nulla non ipsa porro et culpa consectetur</p>
+          <div className="blog-card--info">
+            <p className="blog-card--date">12/12/2020</p>
+            <p className="blog-card--read-more">Lire la suite...</p>
+          </div>
         </a>
       </div>
 
